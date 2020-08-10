9856d666a7310a1d40ad499c527bfd8cfbe8dcae
MMDM <mmdehghan95@gmail.com>
2020-08-10 21:55:02 +0430

    new file added

---
+++ src/Interfaces/IClosureProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Sim\Event\Interfaces;
+
+
+interface IClosureProvider
+{
+    /**
+     * @param $key
+     * @param $closure
+     * @return IClosureProvider
+     */
+    public function addClosure($key, $closure): IClosureProvider;
+
+    /**
+     * @param $key
+     * @return \Closure
+     */
+    public function getClosure($key): \Closure;
+}
