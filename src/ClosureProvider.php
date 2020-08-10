2bcb208aa6476b7a9ecb1b2b4756dee6d237698e
MMDM <mmdehghan95@gmail.com>
2020-08-10 21:54:36 +0430

    new file added

---
+++ src/ClosureProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Sim\Event;
+
+use Sim\Event\Interfaces\IClosureProvider;
+
+class ClosureProvider implements IClosureProvider
+{
+    /**
+     * @var array $closures
+     */
+    protected $closures = [];
+
+    /**
+     * @var \Closure
+     */
+    protected $noop;
+
+    /**
+     * ClosureProvider constructor.
+     */
+    public function __construct()
+    {
+        $this->noop = function () {
+        };
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addClosure($key, $closure): IClosureProvider
+    {
+        $this->closures[$key] = $closure;
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getClosure($key): \Closure
+    {
+        return $this->closures[$key] ?? $this->noop;
+    }
+}
